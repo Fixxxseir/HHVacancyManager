3610565c8db9435f32bb55a8f78c7444e7909195
mexic <mexic2284@mail.ru>
2024-10-13 02:37:53 +0300

    moduls for base classes abstractmethods

---
+++ src/base_vacancy_data_manager.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+
+from src.vacancy import Vacancy
+
+
+class VacancySaver(ABC):
+
+    @abstractmethod
+    def __read_file(self):
+        pass
+
+    @abstractmethod
+    def __write_to_file(self, vacancies):
+        pass
+
+    @abstractmethod
+    def add_vacancy(self, vacancy: Vacancy) -> None:
+        """Добавляет вакансию объекта Vacancy"""
+        pass
+
+    @abstractmethod
+    def add_vacancies(self, vacancies: list[Vacancy]) -> None:
+        """Добавляет список объектов Vacancy"""
+
+    @abstractmethod
+    def get_vacancy_by_vacancy_name(self, word: str) -> list[Vacancy]:
+        """Возвращает список вакансий"""
+        pass
+
+    @abstractmethod
+    def delete_vacancy(self, name: str) -> None:
+        """Удаление вакансии из файла"""
+        pass
